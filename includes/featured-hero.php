df9a4534adb20c68caaa4f0cc218f79c3282c18d
Chris Allwood <chris.allwood@un.titled.co.uk>
2013-12-03 01:32:16 +0000

    sub page creation

--- includes/featured-hero.php
+++ includes/featured-hero.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<h1>The Business of <br><span>Web Design</span></h1>
 		<p>The stuff you need to know to<br> run your own show</p>
-		<p>18th July 2013 - Atrium, Cardiff</p>
+		<p><strong>18th July 2013 - Atrium, Cardiff</strong></p>
 		<div class="l-grid-row">
 			<a href="#" class="m-btn btn-secondary">Learn More</a><a href="#" class="m-btn btn-primary">Buy Tickets</a>
 		</div>
---
+++ includes/sub-hero.php
@@ -0,0 +1,6 @@
+<div class="m-hero sub-hero">
+	<img src="assets/img/tbo-hero.jpg" alt="">
+	<div class="container">
+		<h1 class="h2">The Business of <br><span>Web Design</span></h1>
+	</div>
+</div>
