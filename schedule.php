d909502c1c88c5f8622542728f8ab6e6966cc40c
Chris Allwood <chris.allwood@un.titled.co.uk>
2013-12-05 01:31:49 +0000

    schedule page set up and content

---
+++ schedule.php
@@ -0,0 +1,160 @@
+<?php include('includes/header.php'); ?>
+	<section class="site-body">
+		
+		<?php include('includes/sub-hero.php'); ?>
+
+		<article class="m-info-block">
+			<div class="container">
+				<header class="l-grid-10 block-centered">
+					<h2>Conference Schedule</h2>
+					<p class="archer-italic">At the heart of The Business of Web Design is the venue, taking place in a The Atrium, Cardiff.</p>
+				</header>
+				<div class="l-grid-row">
+					<ul class="m-inline-list m-schedule">
+						<li class="schedule-item">
+							<h2>8.00am</h2>
+							<img src="assets/content/sample-speaker.jpg" alt="">
+							<h2>James Young</h2>
+							<p class="archer-italic">Contracts for the Faint Hearted</p>
+							<a class="js-slidetoggle" href="#">Read More</a>
+							<div class="js-slidetoggle-container schedule-details-container l-grid-14">
+								<div class="">
+									<h3>ABOUT WESTLEY</h3>
+									<p>Senior Developer with @thinkbda, blogger, speaker, father and husband. Order of importance not necessarily as stated.</p>
+								</div>
+								<div class="">
+									<h3>CONTRACTS FOR THE FAINT HEARTED</h3>
+									<p>Perhaps you're a freelancer in the web design industry. Or maybe you run your own small dev team.</p>
+									<p>Chances are that you are very clued up with CSS3, RWD etc and very passionate about creating great work for your clients. </p>
+								</div>
+							</div>
+						</li>
+
+						<li class="schedule-item">
+							<h2>8.00am</h2>
+							<img src="assets/content/sample-speaker.jpg" alt="">
+							<h2>James Young</h2>
+							<p class="archer-italic">Contracts for the Faint Hearted</p>
+							<a class="js-slidetoggle" href="#">Read More</a>
+							<div class="js-slidetoggle-container schedule-details-container l-grid-14">
+								<div class="">
+									<h3>ABOUT WESTLEY</h3>
+									<p>Senior Developer with @thinkbda, blogger, speaker, father and husband. Order of importance not necessarily as stated.</p>
+								</div>
+								<div class="">
+									<h3>CONTRACTS FOR THE FAINT HEARTED</h3>
+									<p>Perhaps you're a freelancer in the web design industry. Or maybe you run your own small dev team.</p>
+									<p>Chances are that you are very clued up with CSS3, RWD etc and very passionate about creating great work for your clients. </p>
+								</div>
+							</div>
+						</li>
+
+						<li class="schedule-item">
+							<h2>8.00am</h2>
+							<h3>James Young</h3>
+							<p class="archer-italic">Sammiches</p>
+						</li>
+						
+					</ul>
+				</div>
+			</div>
+		</article>
+
+		
+
+		<article class="m-info-block d-highlight">
+			<div class="container">
+				<header>
+					<h2>The Sponsors</h2>
+				</header>
+				<div class="l-grid-row">
+					<ul class="m-inline-list sponsor tier-1">
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-3 p-grid-2 t-grid-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail.jpg" alt="">
+							</a>
+						</li>
+					</ul>
+					<ul class="m-inline-list sponsor tier-2">
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1 l-clear-2">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+						<li class="tile-item l-grid-2 p-grid-third t-grid-1">
+							<a href="#">
+								<img src="assets/img/sponsor-thumbnail-small.jpg" alt="">
+							</a>
+						</li>
+					</ul>
+				</div>
+				<footer>
+					<p>Interested in sponsorship opportunities at The Business of Web Design?</p>
+					<a href="#"><em>Take a look at our sponsorship pack.</em></a>
+				</footer>
+			</div>
+		</article>
+
+		<article class="m-info-block sign-up-cta centered extended-block">
+			<div class="container">
+				<h2 class="h1">The Business of <span>Web Design</span></h2>
+				<p>Help & advice from the front line of running 
+a web design business</p>
+				<p><strong>18th July 2013 - Atrium, Cardiff</strong></p>
+
+				<a href="#" class="m-btn btn-primary">Buy Tickets</a>
+			</div>
+		</article>
+
+	</section>
+<?php include('includes/footer.php'); ?>
